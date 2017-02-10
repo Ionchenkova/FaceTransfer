8d5160da5d47e0e17bcfab6408e0ef6afc90e7ac
Maria Ionchenkova <IosDeveloperIon@gmail.com>
2017-02-10 12:56:55 +0300

    Changed saving image

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import sys
 
 imagePath = sys.argv[1]
 cascPath = sys.argv[2]
+fileToSaveImage = sys.argv[3]
 
 face = facedetect.getFace(imagePath, cascPath, 30)
+
 if face is None:
     print('Found 0 or >1 faces')
-# else:
-#     cv2.imshow("cropped", face)
-#     cv2.waitKey(0)
+
+cv2.imwrite(fileToSaveImage, face)
