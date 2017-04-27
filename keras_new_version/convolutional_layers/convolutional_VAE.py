3bb2d2a91ce42bc1eca17548181438cace8a7cec
Maria Ionchenkova <IosDeveloperIon@gmail.com>
2017-04-27 19:58:18 +0300

    Added option for the convolutional network. But it work bad.

---
+++ keras_new_version/convolutional_layers/convolutional_VAE.py
@@ -0,0 +1,166 @@
+from keras.layers import Input, Dense, Lambda, Flatten, Reshape
+from keras.layers import Conv2D, AveragePooling2D, UpSampling2D, Conv2DTranspose
+from keras.models import Model
+from keras import backend as K
+from keras import metrics
+from scipy import misc
+from glob import glob
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Settings:
+    img_size_rows = 64
+    img_size_cols = 64
+    img_size_chnl = 1
+    full_img_size = (img_size_rows, img_size_cols, img_size_chnl)
+    batch_size = 1
+    latent_dim = 2
+    intermediate_dim = 128
+    epsilon = 1.0
+    num_of_epoch = 100
+    log = True
+
+def load_image(image_path):
+    grayImage = misc.imread(image_path, mode="L")
+    image = grayImage.reshape((1,) + grayImage.shape)
+    image = image.astype('float32') / 255.
+    image = image.reshape((image.shape[0],) + Settings.full_img_size)
+    return image
+
+def show_images(decoder, n=10, img_size=Settings.img_size_rows):
+    figure = np.zeros((img_size * n, img_size * n))
+    grid_x = np.linspace(-1.0, 1.0, n)
+    grid_y = np.linspace(-1.0, 1.0, n)
+    for i, yi in enumerate(grid_x):
+        for j, xi in enumerate(grid_y):
+            z_sample = np.array([[xi, yi]]) * Settings.epsilon
+            x_decoded = decoder.predict(z_sample) # we ganerate latent layer with latent_dim
+            image = x_decoded[0].reshape(img_size, img_size)
+            figure[i * img_size: (i + 1) * img_size,
+                   j * img_size: (j + 1) * img_size] = image
+    plt.figure(figsize=(6, 6))
+    plt.imshow(figure, cmap='Greys_r')
+    plt.show()
+
+class VAE:
+    def __init__(self):
+        # CODER
+        self.input_layer = Input(shape=Settings.full_img_size)
+        self.conv_layer_1 = Conv2D(filters=8, 
+                                    kernel_size=(3, 3), 
+                                    padding='same', 
+                                    activation='relu')(self.input_layer) # (64,64,1)
+        self.avrg_pool_layer_1 = AveragePooling2D(pool_size=(2, 2))(self.conv_layer_1) # (32,32,1)
+        self.conv_layer_2 = Conv2D(filters=16, 
+                                    kernel_size=(3, 3), 
+                                    padding='same', 
+                                    activation='relu')(self.avrg_pool_layer_1) # (32,32,16)
+        self.avrg_pool_layer_2 = AveragePooling2D(pool_size=(2, 2))(self.conv_layer_2) # (16,16,16)
+        self.conv_layer_3 = Conv2D(filters=16,
+                                    kernel_size=(3, 3),
+                                    padding='same', 
+                                    activation='relu')(self.avrg_pool_layer_2) # (16,16,16)
+        self.avrg_pool_layer_3 = AveragePooling2D(pool_size=(2, 2))(self.conv_layer_3) # (8,8,16)
+        self.conv_layer_4 = Conv2D(filters=16,
+                                    kernel_size=(3, 3),
+                                    padding='same', 
+                                    activation='relu')(self.avrg_pool_layer_3) # (8,8,8)
+        self.flat_layer = Flatten()(self.conv_layer_4)
+        self.hidden_layer = Dense(Settings.intermediate_dim, activation='relu')(self.flat_layer)
+        self.z_mean = Dense(Settings.latent_dim)(self.hidden_layer)
+        self.z_random = Dense(Settings.latent_dim)(self.hidden_layer)
+        self.z = Lambda(self.sampling)([self.z_mean, self.z_random])
+        # Just layers for decoder
+        arr_size_conv_layer_4 = [i.value for i in self.conv_layer_4.shape.dims if i.value is not None]
+        size_flat_layer = np.prod(arr_size_conv_layer_4)
+        size_conv_layer_4 = tuple(arr_size_conv_layer_4)
+
+        self.decoder_hidden = Dense(Settings.intermediate_dim, activation='relu')
+        self.decoder_flat = Dense(size_flat_layer, activation='relu')
+        self.decoder_reshape = Reshape(size_conv_layer_4)
+        self.decoder_conv_4 = Conv2D(filters=16,
+                                    kernel_size=(3, 3),
+                                    padding='same',
+                                    activation='relu')
+        self.up_sampling_conv_4 = UpSampling2D()
+        self.decoder_conv_3 = Conv2D(filters=16,
+                                    kernel_size=(3, 3),
+                                    padding='same',
+                                    activation='relu')
+        self.up_sampling_conv_3 = UpSampling2D()
+        self.decoder_conv_2 = Conv2D(filters=16,
+                                    kernel_size=(3, 3),
+                                    padding='same',
+                                    activation='relu')
+        self.up_sampling_conv_2 = UpSampling2D()
+        self.decoder_conv_1 = Conv2D(filters=1,
+                                    kernel_size=(3, 3),
+                                    padding='same',
+                                    activation='sigmoid')
+        # DECODER (connected with full VAE)
+        self.hidden_decoded = self.decoder_hidden(self.z)
+        self.flat_decoded = self.decoder_flat(self.hidden_decoded)
+        self.reshape_decoded = self.decoder_reshape(self.flat_decoded)
+        self.conv_4_decoded = self.decoder_conv_4(self.reshape_decoded)
+        self.up_sampling_conv_4_decoded = self.up_sampling_conv_4(self.conv_4_decoded)
+        self.conv_3_decoded = self.decoder_conv_3(self.up_sampling_conv_4_decoded)
+        self.up_sampling_conv_3_decoded = self.up_sampling_conv_3(self.conv_3_decoded)
+        self.conv_2_decoded = self.decoder_conv_2(self.up_sampling_conv_3_decoded)
+        self.up_sampling_conv_2_decoded = self.up_sampling_conv_2(self.conv_2_decoded)
+        self.conv_1_decoded = self.decoder_conv_1(self.up_sampling_conv_2_decoded)
+
+    def sampling(self, hidden_layers):
+        z_mean, z_random = hidden_layers
+        normal = K.random_normal(shape=(Settings.batch_size, Settings.latent_dim),
+                                 mean=0.,
+                                 stddev=Settings.epsilon)
+        return z_mean + z_random * normal
+    
+    def loss(self, x, x_decoded_mean):
+        x = K.flatten(x)
+        x_decoded_mean = K.flatten(x_decoded_mean)
+        xent_loss = Settings.full_img_size * metrics.binary_crossentropy(x, x_decoded_mean)
+        kl_loss = - 0.5 * K.sum(1 + self.z_random - K.square(self.z_mean) - K.exp(self.z_random), axis=-1)
+        return xent_loss + kl_loss
+    
+    def get_VAE_model(self):
+        return Model(self.input_layer, self.conv_1_decoded)
+    
+    def get_encoder_model(self):
+        return Model(self.input_layer, self.z_mean)
+    
+    def get_decoder_model(self):
+        decoder_input = Input(shape=(Settings.latent_dim,))
+        hidden_decoded = self.decoder_hidden(decoder_input)
+        flat_decoded = self.decoder_flat(hidden_decoded)
+        reshape_decoded = self.decoder_reshape(flat_decoded)
+        conv_4_decoded = self.decoder_conv_4(reshape_decoded)
+        up_sampling_conv_4_decoded = self.up_sampling_conv_4(conv_4_decoded)
+        conv_3_decoded = self.decoder_conv_3(up_sampling_conv_4_decoded)
+        up_sampling_conv_3_decoded = self.up_sampling_conv_3(conv_3_decoded)
+        conv_2_decoded = self.decoder_conv_2(up_sampling_conv_3_decoded)
+        up_sampling_conv_2_decoded = self.up_sampling_conv_2(conv_2_decoded)
+        conv_1_decoded = self.decoder_conv_1(up_sampling_conv_2_decoded)
+        return Model(decoder_input, conv_1_decoded)
+
+if __name__ == "__main__":
+    
+    images = glob("/Users/Maria/Documents/input_faces/*.jpg")
+    load_x_train = [load_image(image) for image in images]
+    x_train = np.concatenate(load_x_train)
+    
+    vae = VAE()
+    vae_model = vae.get_VAE_model()
+    encoder = vae.get_encoder_model()
+    decoder = vae.get_decoder_model()
+    
+    vae_model.compile(optimizer='rmsprop', loss=vae.loss)
+    vae_model.summary() # log
+    vae_model.fit(x_train, x_train,
+                  shuffle=True,
+                  epochs=Settings.num_of_epoch,
+                  batch_size=Settings.batch_size,
+                  validation_data=(x_train, x_train))
+                  
+    show_images(decoder, n=20)
