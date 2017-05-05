65233ede4b121cae4c0b28b8aceab75213fe143e
Maria Ionchenkova <IosDeveloperIon@gmail.com>
2017-05-05 13:58:20 +0300

    Added print matrix as func

--- keras_new_version/main.py
+++ keras_new_version/main.py
@@ -1,6 +1,7 @@
 import densely_connected_layers.VAE_densely_connected as dense_VAE
 import convolutional_layers.convolutional_VAE as conv_VAE
 import discriminant_model as dis_model
+from sklearn.metrics import classification_report,confusion_matrix
 import sys
 import keras
 import numpy as np
@@ -10,7 +11,23 @@ from glob import glob
 # 1 is real
 # 0 is fake
 
-EPOCH = 200
+EPOCH_VAE = 1
+EPOCH_DIS = 1
+
+def printMetrix(model, x_test, y_test_labels):
+    Y_pred = model.predict(x_test)
+    print(Y_pred)
+    y_pred = np.floor(Y_pred + np.array(0.5)).ravel() # np.argmax(Y_pred, axis=1)
+    print(y_pred)
+    y_pred = model.predict_classes(x_test)
+    print(y_pred.ravel())
+    p = model.predict_proba(x_test) # to predict probability
+    target_names = ['class 0(REAL)', 'class 1(FAKE)']
+    print(classification_report(y_test_labels, y_pred, target_names=target_names))
+    # Tp | Fn
+    #---------
+    # Fp | Tn
+    print(confusion_matrix(y_test_labels, y_pred))
 
 # CONV
 
@@ -34,7 +51,7 @@ all_real_images_conv = np.concatenate([x_train_conv, x_test_conv]) # all real im
 
 #--------------------
 
-conv_VAE.Settings.num_of_epoch = EPOCH
+conv_VAE.Settings.num_of_epoch = EPOCH_VAE
 
 vae = conv_VAE.VAE()
 vae_model = vae.get_VAE_model()
@@ -67,8 +84,11 @@ for i, yi in enumerate(grid_x):
         img = img.reshape((img.shape[0],) + conv_VAE.Settings.full_img_size) # (1,64,64,1)
         generated_images_conv.append(img)
 
+print 'generated_images_conv len is ', len(generated_images_conv)
 fake_cnn_train = generated_images_conv[0:24] # 24 images
+print 'fake_cnn_train len is ', len(fake_cnn_train)
 fake_cnn_test = generated_images_conv[-25:] # 25 images
+print 'fake_cnn_test len is ', len(fake_cnn_test)
 
 fake_cnn_train = np.concatenate(fake_cnn_train)
 fake_cnn_test = np.concatenate(fake_cnn_test)
@@ -83,7 +103,7 @@ x_test_dense = np.concatenate(load_x_test_dense)
 
 all_real_images_dense = np.concatenate([x_train_dense, x_test_dense])
 
-dense_VAE.Settings.num_of_epoch = EPOCH
+dense_VAE.Settings.num_of_epoch = EPOCH_VAE
 
 vae = dense_VAE.VAE()
 vae_model = vae.get_VAE_model()
@@ -113,7 +133,6 @@ for i, yi in enumerate(grid_x):
         img = x_decoded[0].reshape(img_size, img_size) # (64,64)
         img = img.astype('float32') / 255.
         img = img.reshape((1,) + img.shape + (1,)) # (1,64,64,1)
-        print 'img.shape is ', img.shape
         generated_images_dense.append(img)
 
 fake_fn_train = generated_images_dense[0:24] # 24 train images
@@ -122,32 +141,34 @@ fake_fn_test = generated_images_dense[-25:] # 25 test images
 fake_fn_train = np.concatenate(fake_fn_train)
 fake_fn_test = np.concatenate(fake_fn_test)
 
-#-----------
-# TRAINING CNN MODEL
+print('-------------------------------------------------- CNN ------------------------------------------------')
 
-y_train_real = [1] * 25
-y_fake_cnn = [0] * 24
+y_train_real = [0.] * 25
+y_fake_cnn = [1.] * 24
 y_train_labels_cnn = y_train_real + y_fake_cnn # real + fake
 
 x_train_cnn = np.concatenate([x_train_conv, fake_cnn_train])
 
-dis_model.epochs = EPOCH
+dis_model.epochs = EPOCH_DIS
 model = dis_model.load(x_train=x_train_cnn, y_train=y_train_labels_cnn, x_test=x_train_cnn, y_test=y_train_labels_cnn)
 
 x_test_cnn = np.concatenate([x_test_conv, fake_cnn_test])
-y_test_labels_cnn = y_train_real + [0] * 25
-y_test_labels_cnn = keras.utils.to_categorical(y_test_labels_cnn, 2)
+y_test_labels_cnn = y_train_real + [1.] * 25
 
 test_score_conv = model.evaluate(x_test_cnn, y_test_labels_cnn, verbose=0) # for test
 print('Test CONV loss:', test_score_conv[0])
 print('Test CONV accuracy:', test_score_conv[1])
 print("Baseline CONV Error: %.2f%%" % (100-test_score_conv[1]*100))
 
-#-----------
-# TRAINING FN MODEL
+print('-------------------------------------------- CNN METRICS -----------------------------------------------')
 
-y_train_real = [1] * 25
-y_fake_fn = [0] * 24
+printMetrix(model, x_test_cnn, y_test_labels_cnn)
+
+print('--------------------------------------------------------------------------------------------------------')
+print('-------------------------------------------------- FN --------------------------------------------------')
+
+y_train_real = [0.] * 25
+y_fake_fn = [1.] * 24
 y_train_labels_fn = y_train_real + y_fake_fn # real + fake
 
 print 'x_train_dense.shape is ', x_train_conv.shape
@@ -155,17 +176,22 @@ print 'fake_fn_train.shape is ', fake_fn_train.shape
 
 x_train_fn = np.concatenate([x_train_conv, fake_fn_train])
 
-dis_model.epochs = EPOCH
+dis_model.epochs = EPOCH_DIS
 model = dis_model.load(x_train=x_train_fn, y_train=y_train_labels_fn, x_test=x_train_fn, y_test=y_train_labels_fn)
 
 x_test_fn = np.concatenate([x_test_conv, fake_fn_test])
-y_test_labels_fn = y_train_real + [0] * 25
-y_test_labels_fn = keras.utils.to_categorical(y_test_labels_fn, 2)
+y_test_labels_fn = y_train_real + [1.] * 25
+#y_test_labels_fn = keras.utils.to_categorical(y_test_labels_fn, 2)
 
 test_score_conv = model.evaluate(x_test_fn, y_test_labels_fn, verbose=0) # for test
 print('Test FN loss:', test_score_conv[0])
 print('Test FN accuracy:', test_score_conv[1])
 print("Baseline FN Error: %.2f%%" % (100-test_score_conv[1]*100))
 print(model.metrics_names)
+print('--------------------------------------------------------------------------------------------------------')
+print('-------------------------------------------- FN METRICS -----------------------------------------------')
 
+printMetrix(model, x_test_fn, y_test_labels_fn)
+
+print('--------------------------------------------------------------------------------------------------------')
 
